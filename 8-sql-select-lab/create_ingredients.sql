32aec83a68ad290d7b0a034ed27627b8a3940b2d
Brian Srebrenik <briansrebrenik@gmail.com>
2019-10-16 10:29:51 -0400

    sql select lab

---
+++ 8-sql-select-lab/create_ingredients.sql
@@ -0,0 +1,2 @@
+CREATE TABLE ingredients (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT,
+  cost_per_ounce FLOAT, calories_per_ounce INTEGER, expiration DATE);
---
+++ 8-sql-select-lab/insert_employees.sql
@@ -0,0 +1,13 @@
+INSERT INTO employees (name, position, start_date, zip_code, phone_number)
+VALUES ("Tom", "manager", "2019-05-20", "10001", "212-438-3984"),
+  ("Alison", "salad maker", "2020-01-04", "10023", "212-329-2489"),
+  ("Jerry", "delivery driver", "2019-12-15", "10001", "212-219-1029"),
+  ("Brian", "salad maker", "2019-12-04", "10002", "646-328-2309"),
+  ("Vanessa", "clerk", "2019-11-14", "11201", "718-348-2348"),
+  ("Cindy", "assistant manager", "2019-11-12", "10002", "212-300-1203"),
+  ("Charles", "delivery driver", "2020-01-02", "10003", "212-993-2348"),
+  ("Alex", "assistant manager", "2019-06-11", "10001", "718-293-1823"),
+  ("Naomi", "manager", "2019-02-12", "10002", "212-493-2922"),
+  ("Isabel", "salad maker", "2020-01-04", "10001", "212-390-9122"),
+  ("James", "clerk", "2020-01-10", "10023", "718-645-2929"),
+  ("James", "salad maker", "2019-10-03", "10002", "212-555-2911");
